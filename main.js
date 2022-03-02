4e19c50ba291186844dfc5c8d12ceceeacafc8dd
vadivelan1998 <vadivelan1998@gmail.com>
2022-03-02 22:12:49 +0530

    calculator

---
+++ main.js
@@ -0,0 +1,11 @@
+const add=require("./add") 
+console.log(add(2,3))
+
+const div=require("./divide")
+console.log(div(2,2))
+
+const mul=require("./multiply") 
+console.log(mul(2,3))
+
+const sub=require("./subtract.js")
+console.log(sub(5,3))
